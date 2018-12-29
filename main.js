f72bcb892897cb70cabf4796dc1e21ef3768b80b
Jelle Herold <jelle@defekt.nl>
2018-12-29 22:06:47 +0100

    hi

---
+++ main.js
@@ -0,0 +1,110 @@
+// pull URL from the web
+const grab = s => fetch(s).then(x => x.text())
+
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+ 
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+ 
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+async function main(selector) {
+  const canvas = document.querySelector(selector);
+  
+  // Initialize the GL context
+  const gl = canvas.getContext("webgl");
+
+  // Only continue if WebGL is available and working
+  if (gl === null) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
+
+  const vertexShaderSource = await grab("vertex.glsl");
+  const fragmentShaderSource = await grab("vhs.glsl");
+
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+  var program = createProgram(gl, vertexShader, fragmentShader);
+
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // three 2d points
+  var positions = new Float32Array([
+    0, 0,
+    0, 0.5,
+    0.7, 0,
+  ]);
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+
+  // resize to actual screen size
+  resize(gl);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 0.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // use the shaders
+  gl.useProgram(program);
+
+  // turn attribute on and specify how to pull data out of it
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+ 
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  let size = 2;          // 2 components per iteration
+  let type = gl.FLOAT;   // the data is 32bit floats
+  let normalize = false; // don't normalize the data
+  let stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  let offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
+
+  let primitiveType = gl.TRIANGLES;
+  let count = 3;
+  gl.drawArrays(primitiveType, offset, count);  
+}
+
+function resize(gl) {
+  var realToCSSPixels = window.devicePixelRatio;
+
+  // Lookup the size the browser is displaying the canvas in CSS pixels
+  // and compute a size needed to make our drawingbuffer match it in
+  // device pixels.
+  var displayWidth  = Math.floor(gl.canvas.clientWidth  * realToCSSPixels);
+  var displayHeight = Math.floor(gl.canvas.clientHeight * realToCSSPixels);
+
+  // Check if the canvas is not the same size.
+  if (gl.canvas.width  !== displayWidth ||
+      gl.canvas.height !== displayHeight) {
+
+    // Make the canvas the same size
+    gl.canvas.width  = displayWidth;
+    gl.canvas.height = displayHeight;
+  }
+}
---
+++ vhs.glsl
@@ -0,0 +1,66 @@
+precision mediump float;
+
+float iTime = 0.0;
+
+float rand(vec2 co) {
+    return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+float noise(float prev, in vec2 fragCoord) {
+    float noiseS = sin(8.0 * fragCoord.y);
+    return prev * 0.9 + 0.1 * noiseS;
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+    float y = 0.1 * fragCoord.y + 40.0 * iTime;
+    float n = pow(sin(y * 0.1) * 0.6 + 0.6, 5.0);
+    vec4 stripes = vec4(0.9 * n, 0.3 * n, 0.97 * n, 1.0);
+
+    float noize = pow(rand(iTime / 5000.0 + fragCoord), 6.0);
+    noize = noise(noize, fragCoord);
+    noize = noise(noize, fragCoord);
+    noize = noise(noize, fragCoord);
+    noize = noise(noize, fragCoord);
+    noize = noise(noize, fragCoord);
+    noize = noise(noize, fragCoord);
+    noize = noise(noize, fragCoord);
+
+    float noize2 = noise(noize, fragCoord);
+
+	fragColor = (0.9 * noize2 * stripes) + 0.3 * noize;
+}
+
+void trippy(out vec4 fragColor, in vec2 fragCoord) {
+  float time = iTime * 1.0;
+  vec2 uv = (gl_FragCoord.xy / (500.0) - 0.5) * 8.0;
+  vec2 uv0 = uv;
+  float i0 = 1.0;
+  float i1 = 1.0;
+  float i2 = 1.0;
+  float i4 = 0.0;
+  for (int s = 0; s < 7; s++) {
+    vec2 r;
+    r = vec2(cos(uv.y * i0 - i4 + time / i1), sin(uv.x * i0 - i4 + time / i1)) / i2;
+    r += vec2(-r.y, r.x) * 0.3;
+    uv.xy += r;
+
+    i0 *= 1.93;
+    i1 *= 1.15;
+    i2 *= 1.7;
+    i4 += 0.05 + 0.1 * time * i1;
+  }
+  float r = sin(uv.x - time) * 0.5 + 0.5;
+  float b = sin(uv.y + time) * 0.5 + 0.5;
+  float g = sin((uv.x + uv.y + sin(time * 0.5)) * 0.5) * 0.5 + 0.5;
+  fragColor = vec4(r, g, b, 1.0);
+}
+
+void main() {
+    vec4 a = vec4(0,0,0,0);
+    vec4 b = vec4(0,0,0,0);
+
+    mainImage(a, gl_FragCoord.xy);
+    trippy(b, gl_FragCoord.xy);
+
+    gl_FragColor = vec4(a.x + b.x, a.y + b.y, a.z + b.z, 1);
+}
